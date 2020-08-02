f9fa1ddca63544e86de6055e7eccbd6586cbe8e4
Sonali <sonali21031@gmail.com>
2020-08-02 12:55:52 +0530

    Add files via upload
    final

--- app.py
+++ app.py
@@ -8,27 +8,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     transcript = ""
-    if request.method == "POST":  # if we posted the form
+    if request.method == "POST": 
         print("FORM DATA RECEIVED")
-        # here are 2 forms to make sure this file exist :
-        if "file" not in request.files:  # no file exist/ uploaded
-            print(" Debug: Line 1")
+        if "file" not in request.files:  # no file uploaded
+            print(" Cannot find file")
             return redirect(request.url)  # redirect the user to the home page
 
-        file = request.files['file']  # if file exist it will give me that file
-        if file.filename == "":  # if file is blank/empty, return to the main page
-            print(" Debug: Line 2")
+        file = request.files['file']  # if file exist, give that file
+        if file.filename == "":  # if file is empty, return to the main page
+            print(" Uploaded file is empty")
             return redirect(request.url)
 
         if file:
-            print(" Debug: Line 3")
+            print(" Your file successfully uploaded")
             recognizer = sr.Recognizer()  # initilaize instance of the speech recognition class
             audioFile = sr.AudioFile(file)  # pass in the file
-            with audioFile as source:  # reading the file
-                data = recognizer.record(source)  # through the recognizer
-            transcript = recognizer.recognize_google(data, key=None)  # using Google API will return the text
+            with audioFile as source:  # read the file
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)  # using Google API
 
     return render_template('index.html', transcript=transcript)
 
 if __name__ == '__main__':
-    app.run(port='2207', debug=True, threaded=True)  # multiple requests at the ame time for the file
+    app.run(port='2207', debug=True, threaded=True)  # multiple requests at the same time for the file
